cc01fbaefca1e2d994558088bb74164f3df941bd
Carlos Escorche <carlosescorchep@gmail.com>
2023-01-04 08:57:58 -0300

    Adds handler to connect to the websocket

---
+++ handlers/handler_list_observe.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/carlosescorche/qrlist/server"
+	"github.com/carlosescorche/qrlist/utils/api"
+	"github.com/gorilla/mux"
+)
+
+func HandlerListObserve(s *server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		listId := vars["id"]
+
+		socket, err := server.Upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			http.Error(w, "Error upgrading connection", http.StatusInternalServerError)
+			return
+		}
+
+		client := server.NewClient(listId, s.Hub, socket)
+		s.Hub.Register <- client
+
+		go client.Write()
+
+		api.Success(w, nil, http.StatusOK)
+	}
+}
